17878cba4dbc9248dd61b60db15091895688a511
oliver <mahdig502@gmail.com>
2022-05-09 13:15:22 +0430

    Configuration of tailwindCSS

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <p className="text-3xl font-bold underline ">Test TW</p>
+    </div>
+  );
+};
+
+export default Header;
