30af5453dd4d672bd250173b094b025617c9829a
oliver <mahdig502@gmail.com>
2022-05-09 14:04:21 +0430

    Complete header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import logo from "../assets/logo.png";
+import { HiMenuAlt3 } from "react-icons/hi";
 
 const Header = () => {
   return (
-    <div>
-      <p className="text-3xl font-bold underline ">Test TW</p>
+    <div className="w-full h-20 bg-white shadow-md fixed top-0 flex justify-between items-center">
+      <img className="w-32 h-16 ml-8" src={logo} alt="Logo" />
+      <HiMenuAlt3 className="w-10 h-10 m-4 mr-8" />
     </div>
   );
 };
