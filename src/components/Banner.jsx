594df6a077091329997536b49f60f8566635dc0a
oliver <mahdig502@gmail.com>
2022-05-09 14:26:58 +0430

    Complete banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="w-full relative top-20 bg-slate-100 flex justify-center">
+      <p className="text-3xl mt-14">
+        We are a creative groups who works with ad game and ...
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
