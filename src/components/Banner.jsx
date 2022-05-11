693272db3a2cbdd627452a8791c434e00493059e
oliver <mahdig502@gmail.com>
2022-05-11 20:16:07 +0430

    Add mobiles into banner component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import mobile1 from "../assets/Mobiles/mobile1.png";
+import mobile2 from "../assets/Mobiles/mobile2.png";
+import mobile3 from "../assets/Mobiles/mobile3.png";
 
 const Banner = () => {
   return (
-    <div className="w-full mt-20 pb-20 bg-slate-100 flex justify-center">
-      <h2 className="text-3xl mt-14">
+    <div className="w-full mt-20 pb-20 bg-slate-100 flex flex-col justify-center">
+      <h2 className="text-3xl mt-14 mb-36 text-center ">
         We are a creative groups who works with ad game and ...
       </h2>
+      <div className="w-full flex justify-center m-auto gap-16">
+        <img style={{width:145, height:348}} className="" src={mobile2} alt="mobile1" />
+        <img style={{width:195, height:357}} className="" src={mobile1} alt="mobile1" />
+        <img style={{width:145, height:348}} className="" src={mobile3} alt="mobile1" />
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    // spacing: {
+    //   "142": "142px",
+    //   "341": "341px",
+    //   "195": "195px",
+    //   "347": "347px",
+    // },
     extend: {
       colors: {
         primary: "#F7AE04",
@@ -10,9 +16,6 @@ module.exports = {
       boxShadow: {
         "my-shadow":
           "0 0px 15px -3px rgb(0 0 0 / 0.2), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
-      },
-      rotate: {
-        360: "360deg",
       },
     },
   },
